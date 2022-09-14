d8c0c53537f364e78a8397c85b980fb9cd1bc903
Xyphuz <hansen0715.2@gmail.com>
2022-09-14 23:31:48 +0800

    chore: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Xyphuz - Official Website</title>
+	<meta name="description" content="This is the official website of Xyphuz." />
+</svelte:head>
+
+<section>
+</section>
+
+<style>
+</style>
