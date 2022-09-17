de7c3be6941542e3b7cb1c799ad942a126cf7d16
Xyphuz <hansen0715.2@gmail.com>
2022-09-17 11:45:34 +0800

    feat: fade in carousel

--- src/routes/projects/[url]/+page.svelte
+++ src/routes/projects/[url]/+page.svelte
@@ -1,104 +1,114 @@
 <script lang="ts">
-    import type { PageData } from './$types';
-    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-    import type { Options } from '@splidejs/splide';
-    import '@splidejs/svelte-splide/css';
+	import type { PageData } from "./$types";
+	import { Splide, SplideSlide } from "@splidejs/svelte-splide";
+	import type { Options } from "@splidejs/splide";
+	import "@splidejs/svelte-splide/css";
 
 	export let data: PageData;
 
-    const splideOptions: Options = {
-        type: "loop",
-        width: "100%",
-        height: "25em",
-        autoWidth: true,
-        focus: "center",
-        autoplay: true,
-        interval: 3000,
-        speed: 1000,
-        lazyLoad: true,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-    };
+	const splideOptions: Options = {
+		type: "loop",
+		width: "100%",
+		height: "25em",
+		autoWidth: true,
+		focus: "center",
+		autoplay: true,
+		interval: 3000,
+		speed: 1000,
+		lazyLoad: true,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+	};
+
+	let splideOpacity = 0;
+	setTimeout(() => {
+		splideOpacity = 1;
+	}, 500);
 </script>
 
-<section>
-    {#if data.project.attributes.photoUrls != null}
-        <Splide options={splideOptions}>
-            {#each data.project.attributes.photoUrls as photoUrl}
-                <SplideSlide>
-                    <img src={photoUrl} loading="lazy" alt="{photoUrl}" style="height: 25em" />
-                </SplideSlide>
-            {/each}
-        </Splide>
-    {/if}
+<section style="--splide-opacity: {splideOpacity}">
+	{#if data.project.attributes.photoUrls != null}
+		<Splide options={splideOptions}>
+			{#each data.project.attributes.photoUrls as photoUrl}
+				<SplideSlide>
+					<img src={photoUrl} loading="lazy" alt={photoUrl} style="height: 25em" />
+				</SplideSlide>
+			{/each}
+		</Splide>
+	{/if}
 
-    <div id="article-container">
-        <article>
-            <h1 id="title">{data.project.attributes.title}</h1>
-            <div id="date">{data.project.attributes.dateString}</div>
-            <hr>
-            {@html data.project.html}
-        </article>
-    </div>
+	<div id="article-container">
+		<article>
+			<h1 id="title">{data.project.attributes.title}</h1>
+			<div id="date">{data.project.attributes.dateString}</div>
+			<hr />
+			{@html data.project.html}
+		</article>
+	</div>
 </section>
 
 <style>
-    section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-
-    #article-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 4em 2.5em 2em 2.5em;
-    }
-    
-	article :global(img) {
-        max-width: 100%;
-	}    
-
-	#title {
-        margin-top: 0.25em;
-        margin-bottom: 1.25em;
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 	}
 
-    hr {
-        margin-top: 1.2em;
-        margin-bottom: 3em;
-        border: 1px solid #eee;
-    }
+	section :global(.splide) {
+		transition: opacity 1s ease-in-out;
+		opacity: var(--splide-opacity);
+	}
 
-    @media (orientation: landscape) {
-        article {
-            width: 50%;
-        }
-        
-        article :global(h1) {
-            font-size: 2.4rem;
-        }
-    }
+	#article-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 4em 2.5em 2em 2.5em;
+	}
 
-    @media (orientation: portrait) {
-        article {
-            width: 100%;
-        }
+	article :global(img) {
+		max-width: 100%;
+	}
 
-        article :global(h1) {
-            font-size: 2rem;
-        }
-    }
+	#title {
+		margin-top: 0.25em;
+		margin-bottom: 1.25em;
+	}
 
-    :global(.splide__slide) {
-        opacity: 0.2;
-        transition: all 0.25s ease-in-out;
-    }
+	hr {
+		margin-top: 1.2em;
+		margin-bottom: 3em;
+		border: 1px solid #eee;
+	}
 
-    :global(.splide__slide.is-active) {
-        opacity: 1;
-    }
+	@media (orientation: landscape) {
+		article {
+			width: 50%;
+		}
+
+		article :global(h1) {
+			font-size: 2.4rem;
+		}
+	}
+
+	@media (orientation: portrait) {
+		article {
+			width: 100%;
+		}
+
+		article :global(h1) {
+			font-size: 2rem;
+		}
+	}
+
+	:global(.splide__slide) {
+		opacity: 0.2;
+		transition: all 0.25s ease-in-out;
+	}
+
+	:global(.splide__slide.is-active) {
+		opacity: 1;
+	}
 </style>
