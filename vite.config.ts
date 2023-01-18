40b6bf9ad47932f858309cddcea171ca661a7835
Xyphuz <hansen0715.2@gmail.com>
2023-01-18 22:22:17 +0800

    chore: update threlte dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import type { UserConfig } from "vite";
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ["three", "troika-three-text"],
+	},
 };
 
 export default config;
